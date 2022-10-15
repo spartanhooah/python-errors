67793d2d904d1d8748b4255c456eff25bdaa9bfb
Ben Frey <spartanhooah@gmail.com>
2022-10-15 10:24:23 -0400

    first commit

---
+++ errors.py
@@ -0,0 +1,8 @@
+# Syntax errors
+print(1)
+int(9)
+# SyntaxError at 9 (int is a function, so needs parens)
+# int 9
+print(2)
+# Same as above; print is a function
+# print 3
---
+++ exceptions.py
@@ -0,0 +1,6 @@
+# Exceptions
+a = 1
+b = "2"
+print(int(2.5))
+# Throws TypeError
+# print(a + b)
